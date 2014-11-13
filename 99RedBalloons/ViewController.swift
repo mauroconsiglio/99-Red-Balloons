21c0250f79e816ea375af411820b65ce906e071d
Mauro Consiglio <Mauro@Mauro-Mac-Mini.local>
2014-11-13 15:56:57 +0100

    Initial Commit

---
+++ 99RedBalloons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  99RedBalloons
+//
+//  Created by Mauro Consiglio on 13/11/14.
+//  Copyright (c) 2014 Mauro Consiglio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
